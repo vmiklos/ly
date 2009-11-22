8d3129fb7fb5eed0268644c31b2105c4c1160ef5
Miklos Vajna <vmiklos@frugalware.org>
2009-11-22 13:34:16 +0100

    pdfgen: use pdftk

--- pdfgen.sh
+++ pdfgen.sh
@@ -1,27 +1,8 @@
 #!/bin/sh -e
 
-cwd=$(pwd)
-wd=$(mktemp -d)
-parts=$(grep link: index.txt |sed 's|.*:\(.*\)/.*|\1|')
-( echo '\documentclass[a4paper]{article}'
-echo '\usepackage[a4paper,left=0cm, right=0cm, top=0cm, bottom=0cm]{geometry}'
-echo '\usepackage{graphicx}'
-echo '\begin{document}'
-first=0
-for i in $parts
+in=""
+for i in $(grep link: index.txt |grep -v ly.pdf |sed 's|.*:\(.*\)/.*|\1|')
 do
-	if [ $first = 0 ]; then
-		first=1
-	else
-		echo '\clearpage'
-	fi
-	echo "\includegraphics{$i.eps}"
-	ln -s $cwd/$i/$i.ps $wd/$i.eps
+	in="$in $i/$i.pdf"
 done
-echo '\end{document}') > $wd/ly.tex
-cd $wd
-ln -s ~/git/vmexam/tex/Makefile
-make ly.pdf
-mv ly.pdf $cwd
-cd $cwd
-rm -rf $wd
+pdftk $in cat output ly.pdf
