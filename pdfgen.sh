c40bc5c30f94154e4ac84397689ffe90af81233e
Miklos Vajna <vmiklos@frugalware.org>
2010-01-02 14:33:00 +0100

    pdfgen: generate htaccess files as well

--- pdfgen.sh
+++ pdfgen.sh
@@ -4,5 +4,8 @@ in=""
 for i in $(grep link: index.txt |grep -v ly.pdf |sed 's|.*:\(.*\)/.*|\1|')
 do
 	in="$in $i/$i.pdf"
+	cd $i
+	echo "DirectoryIndex $(pwd|sed 's|.*/||').html" > .htaccess
+	cd -
 done
 pdftk $in cat output ly.pdf
